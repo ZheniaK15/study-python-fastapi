79882c237ce433703176b537916ba07b4d7f8947
ZheniaK15 <164208520+ZheniaK15@users.noreply.github.com>
2024-09-28 22:55:43 +0200

    Add files via upload

---
+++ homework_2.py
@@ -0,0 +1,44 @@
+a = 'hello'
+b = 123 
+c = 145.67
+d = True 
+e = [1, 2, 3, 4] 
+f = {'a': 1, 'b': 2, 'c': 3} 
+g = (1, 2, 3, 4, 5, 6) 
+none = None 
+print( 3 > 5) 
+print(10 == 10) 
+print(3.89 > 2.59) 
+print("adc" == "abc") 
+print("world" != "avfd") 
+print([1, 2, 3] == [1, 2, 3]) 
+print([1, 2, 3] != [4, 5, 6]) 
+print({'a': 1, 'b': 2} == {'a': 1, 'b': 2}) 
+print({'a': 1, 'b': 2} != {'a': 1, 'c': 3}) 
+print((1, 2, 3) == (1, 2, 3)) 
+print((1, 2, 3) != (4, 5, 6)) 
+num_str = 125 
+str(num_str) 
+print(num_str) 
+massege = 'Hi, my name is python' 
+massege = massege.replace('y', '0').replace('i', '1') 
+split_test = 'This is a split test' 
+split_test_list = split_test.split() 
+string_join = ''.join(split_test_list) 
+print(string_join) 
+print(len(string_join)) 
+list_append = [1, 2 ,3] 
+list_append.append(4) 
+list_append.append(5) 
+print(list_append) 
+ist_extend = [4, 5, 6] 
+list1 = [7, 8, 9] 
+list_extend = extend(list1) 
+print(list_extend) 
+print(list_extend.index(6)) 
+print(len(list_append)) 
+dict_test = {'car': 'Toyota', 'price': 4900, 'where': 'EU'} 
+print(dict_test['car'], dict_test['where']) 
+print(dict_test.keys()) 
+print(dict_test.values()) 
+print(dict_test.items())
